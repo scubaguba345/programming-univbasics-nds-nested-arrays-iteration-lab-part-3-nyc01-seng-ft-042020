bef900d2dbf56a0a9e29628f604644941ab5356f
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-03 01:56:38 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,18 +1,18 @@
 def join_nested_strings(src)
-  join_nested = ""
+  join_nested = "" # creating a plce to enter it into
   row_index = 0
-  while row_index < src.count do
+  while row_index < src.count do #while the count of src is greater than 0 do
 
     element_index = 0
     while element_index < src [row_index].count do
-      if src[row_index][element_index].class == String
-        join_nested << src[row_index][element_index] + " "
+      if src[row_index][element_index].class == String   # if the information in src then points to location is equal to a string
+        join_nested << src[row_index][element_index] + " "  #adds them together to combine
       end
-      element_index += 1
+      element_index += 1  #goes through loop again
     end
-    row_index +=1
+    row_index +=1   #goes through loop again
   end
 
-join_nested
+join_nested   #results at end
 
 end
